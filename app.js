28af7fda477f9fbfa6b9891b95234057d7784a7d
lyyeakkhai <yeakhai.ly@student.cadt.edu.kh>
2025-12-30 15:11:44 +0700

    implement database model and middlewares error handling

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ import SubscriptionRoute from './routes/subscription.rounte.js';
 import AuthRoute from './routes/auth.route.js';
 import UserRoute from './routes/user.route.js';
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from './model/middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
+
+
 
 const app = express();
 
 // tell the app that we are going to use json
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// error middleware should be the last middleware
+app.use(errorMiddleware);
+
 app.use('/api/v1/subscription', SubscriptionRoute );
 app.use('/api/v1/auth', AuthRoute );
 app.use('/api/v1/users', UserRoute );
